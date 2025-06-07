1748a8495bb1ddd0723f54835be0154177a36d88
ssuf1998dev <ssuf1998dev@126.com>
2025-06-07 21:52:32 +0800

    feat: ast impl plugins

---
+++ taskfile/ast/plugin_test.go
@@ -0,0 +1,39 @@
+package ast_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v3"
+
+	"github.com/go-task/task/v3/taskfile/ast"
+)
+
+func TestPluginsParse(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		content  string
+		v        any
+		expected any
+	}{
+		{
+			`
+a: a
+b:
+  file: b
+`,
+			&ast.Plugins{},
+			ast.NewPlugins(
+				&ast.PluginElement{Key: "a", Value: &ast.Plugin{File: "a"}},
+				&ast.PluginElement{Key: "b", Value: &ast.Plugin{File: "b"}},
+			),
+		},
+	}
+	for _, test := range tests {
+		err := yaml.Unmarshal([]byte(test.content), test.v)
+		require.NoError(t, err)
+		assert.Equal(t, test.expected, test.v)
+	}
+}
