79e4d050e61ed5d02133f68f4c11aeac0c2439c2
ssuf1998dev <ssuf1998dev@126.com>
2025-06-20 17:01:56 +0800

    feat: cmd interpreter, support js and civet

--- task.go
+++ task.go
@@ -12,9 +12,11 @@ import (
 	"mvdan.cc/sh/v3/interp"
 
 	"github.com/go-task/task/v3/errors"
+	"github.com/go-task/task/v3/experiments"
 	"github.com/go-task/task/v3/internal/env"
 	"github.com/go-task/task/v3/internal/execext"
 	"github.com/go-task/task/v3/internal/fingerprint"
+	"github.com/go-task/task/v3/internal/interpreter"
 	"github.com/go-task/task/v3/internal/logger"
 	"github.com/go-task/task/v3/internal/output"
 	"github.com/go-task/task/v3/internal/slicesext"
@@ -343,16 +345,35 @@ func (e *Executor) runCommand(ctx context.Context, t *ast.Task, call *Call, i in
 		}
 		stdOut, stdErr, closer := outputWrapper.WrapWriter(e.Stdout, e.Stderr, t.Prefix, outputTemplater)
 
-		err = execext.RunCommand(ctx, &execext.RunCommandOptions{
-			Command:   cmd.Cmd,
-			Dir:       t.Dir,
-			Env:       env.Get(t),
-			PosixOpts: slicesext.UniqueJoin(e.Taskfile.Set, t.Set, cmd.Set),
-			BashOpts:  slicesext.UniqueJoin(e.Taskfile.Shopt, t.Shopt, cmd.Shopt),
-			Stdin:     e.Stdin,
-			Stdout:    stdOut,
-			Stderr:    stdErr,
-		})
+		switch (func() string {
+			if experiments.CmdInterpreter.Enabled() {
+				return cmd.Interpreter
+			}
+			return "sh"
+		})() {
+		case "javascript", "js", "civet":
+			err = interpreter.InterpretJS(&interpreter.InterpretJSOptions{
+				Script:  cmd.Cmd,
+				Dialect: cmd.Interpreter,
+				Dir:     t.Dir,
+				Env:     env.GetMap(t),
+				Stdin:   e.Stdin,
+				Stdout:  stdOut,
+				Stderr:  stdErr,
+			})
+		default:
+			err = execext.RunCommand(ctx, &execext.RunCommandOptions{
+				Command:   cmd.Cmd,
+				Dir:       t.Dir,
+				Env:       env.Get(t),
+				PosixOpts: slicesext.UniqueJoin(e.Taskfile.Set, t.Set, cmd.Set),
+				BashOpts:  slicesext.UniqueJoin(e.Taskfile.Shopt, t.Shopt, cmd.Shopt),
+				Stdin:     e.Stdin,
+				Stdout:    stdOut,
+				Stderr:    stdErr,
+			})
+		}
+
 		if closeErr := closer(err); closeErr != nil {
 			e.Logger.Errf(logger.Red, "task: unable to close writer: %v\n", closeErr)
 		}
